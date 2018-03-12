d97352293fb2449ee1b6612a0268e8c80cabb132
MiniBee <hongyue_pei@163.com>
2018-03-12 15:30:23 +0800

    start sparksession 2

--- sparkTest/src/com/phy/DataFrame/DataSetStart.java
+++ sparkTest/src/com/phy/DataFrame/DataSetStart.java
@@ -9,7 +9,7 @@ public class DataSetStart {
 
     private static void createSparkSession(){
         System.setProperty("hadoop.home.dir", "D:\\Tools\\hadoop-2.8.3");
-        SparkSession spark = SparkSession.builder().appName("Java spark sql").getOrCreate();
+        SparkSession spark = SparkSession.builder().appName("Java spark sql").master("local").getOrCreate();
         spark.close();
     }
     public static void main(String[] args) {
